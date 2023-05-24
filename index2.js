73f94214d2ea3c4554d50dc2e7b26025169cf1d8
trilif <68926406+trilif@users.noreply.github.com>
2023-05-25 00:30:24 +0200

    Update index2.js

--- index2.js
+++ index2.js
@@ -4,6 +4,13 @@
 Office.initialize = function(reason) {
   $(document).ready(function(){
     loadEntities();
+	  const map = L.map('map').fitWorld();
+
+	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
+	  
   });
 } 
 
