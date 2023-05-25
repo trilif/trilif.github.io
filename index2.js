4345a4680ca16b37c8794a08c5a20e1003a30c72
trilif <68926406+trilif@users.noreply.github.com>
2023-05-25 12:42:20 +0200

    Update index2.js

--- index2.js
+++ index2.js
@@ -100,7 +100,7 @@ body.getAsync(Office.CoercionType.Text, function (asyncResult) {
       // Note that the use of selectedMatches.mozaikszavak, where
       // OrderNumber corresponds to the RegExName attribute of the Rule element
       // in the manifest
-     $("#selected-match").text(JSON.stringify(selectedMatches.mozaikszavak, null, 2)+"AAK");
+     $("#selected-match").text(JSON.stringify((selectedMatches.mozaikszavak, null, 2)[0])+"AAL");
 	 
 	 var szelvenyString = JSON.stringify(selectedMatches.mozaikszavak, null, 2);
 	 var apstring = szelvenyString.split(" ");
